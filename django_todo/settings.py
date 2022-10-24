b6a7b8354b2a1eb0485367c2f197cc4638e722d2
Rhesmika <Rhesmika@gmail.com>
2022-10-24 20:35:04 +0000

    prepare to deplot to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 if os.path.isfile("env.py"):
     import env
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://bcwbzxxtcuzyja:557f3b09d1b7c212bb139bc297d04b6d8d790b9961c42418e93da99bed363bec@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d65c9sggjcqtaa')
 }
 
 
