a4e3c7746246f88f1cdb385c56e9886701fedb05
Rhesmika <Rhesmika@gmail.com>
2022-10-24 19:54:32 +0000

    add test to edit item on todo list

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -3,8 +3,13 @@ from .models import Item
 
 # Create your tests here.
 
+
 class TestModels(TestCase):
 
     def test_done_defaults_to_false(self):
-        item = Item.objects.create(name= 'Test Todo Item')
+        item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
+
+    def test_model_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
